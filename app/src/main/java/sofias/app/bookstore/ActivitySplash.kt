4ab572166ca9239ed5bb262f21dfe7d15d60d485
Jeffy6917 <32935443+Jeffy6917@users.noreply.github.com>
2019-04-07 18:36:33 -0600

    Commit de inicio

---
+++ app/src/main/java/sofias/app/bookstore/ActivitySplash.kt
@@ -0,0 +1,39 @@
+package sofias.app.bookstore
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class ActivitySplash : AppCompatActivity() {
+    var elhandler : Handler? = null
+
+    val tiempo : Long = 3100
+
+    val corriendo: Runnable = Runnable()
+    {
+        if (!isFinishing)
+        {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        elhandler = Handler()
+        elhandler!!.postDelayed(corriendo, tiempo)
+    }
+    override fun onDestroy()
+    {
+        if (true)
+        {
+            elhandler!!.removeCallbacks(corriendo)
+        }
+        super.onDestroy()
+    }
+
+
+
+}
---
+++ app/src/main/java/sofias/app/bookstore/MainActivity.kt
@@ -0,0 +1,51 @@
+package sofias.app.bookstore
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.BaseExpandableListAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+  //  lateinit var NombreView: EditText
+    //lateinit var GeneroView: EditText
+  //  lateinit var Descripcion: EditText
+
+    //lateinit var buttonGuardar: Button
+    //lateinit var buttonLista: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      //  NombreView = findViewById(R.id.NombreView)
+        //GeneroView = findViewById(R.id.GeneroView2)
+        //Descripcion = findViewById(R.id.DescripcionView3)
+
+
+        //buttonGuardar = findViewById(R.id.buttonGuardar)
+        //buttonLista = findViewById(R.id.buttonLista)
+
+
+
+        //buttonGuardar.setOnClickListener{
+
+        }
+        //buttonLista.setOnClickListener{
+          //  val intent = Intent(this, List:: class.java)
+            //startActivity(intent)
+        }
+
+
+
+
+
+
+
+
